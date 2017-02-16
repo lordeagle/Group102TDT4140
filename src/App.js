b3ab4c1f8d379780a87108e62cd43ecdfa194d7c
lsnellingen <larssnellingen@hotmail.com>
2017-02-16 16:14:47 +0100

    First test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <h1> Testing, testing! </h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
